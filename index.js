289b50078129c9d4cd30c107fba8f085dcade918
BuildTools <unconfigured@null.spigotmc.org>
2020-09-27 13:25:38 +0900

    add clear command

--- index.js
+++ index.js
@@ -7,14 +7,6 @@ client.on('ready', () => {
     console.log('켰다.');
     client.user.setPresence({ game: { name: '모든 우주선 | 접두사: .' }, status: 'online' })
 });
-
-client.on("guildMemberAdd", (member) => {
-    const guild = member.guild;
-    const newUser = member.user;
-    const welcomeChannel = guild.channels.find(channel => channel.name == welcomeChannelName);
-  
-    member.addRole(guild.roles.find(role => role.name == "―――――――성과―――――――"));
-});
       
 client.on("guildMemberAdd", (member) => {
     const guild = member.guild;
@@ -22,15 +14,100 @@ client.on("guildMemberAdd", (member) => {
     const welcomeChannel = guild.channels.find(channel => channel.name == welcomeChannelName);
     let embed = new Discord.RichEmbed()
       .setColor('#F08080')
-      .setDescription(`<@${newUser.id}> 님이 **\`어몽어스 코리아 디스코드\`** 서버에 입장하셨습니다`)
+      .setDescription(`${user.tag} 님이 **\`어몽어스 코리아 디스코드\`** 서버에 입장하셨습니다`)
       
     welcomeChannel.send(embed);
 });
 
+client.on('messageReactionAdd', (reaction, user) => {
+  const { name } = reaction.emoji;
+  const member = reaction.message.guild.members.cache.get(user.id);
+  if (reaction.message.id === '759623973792120842') {
+    switch (name) {
+      case ':Red:':
+        member.roles.add('752475160707072021');
+        break;
+      case ':Orange:':
+        member.roles.add('752493678194327632');
+        break;
+      case ':Yellow:':
+        member.roles.add('752493680517972008');
+        break;
+      case ':Green:':
+        member.roles.add('752493630933041203');
+        break;
+      case ':Lime:':
+        member.roles.add('752493739276238918');
+        break;
+      case ':Blue:':
+        member.roles.add('752493413244338189');
+        break;
+      case ':Cyan:':
+        member.roles.add('752493735878721536');
+        break;
+      case ':Pink:':
+        member.roles.add('752493633625784331');
+        break;
+      case ':Purple:':
+        member.roles.add('752493688302731324');
+        break;
+      case ':White:':
+        member.roles.add('752493685198946374');
+        break;
+      case ':Brown:':
+        member.roles.add('752493732519084095');
+        break;
+      case ':Black:':
+        member.roles.add('752493682711724043');
+        break;
+    
+    }
+  }
+});
 
-client.on('message', (message) => {
-  if(message.content === '.도움') {
-    message.reply('아직 미완성입니다 다음에 이용해주세요');
+client.on('messageReactionRemove', (reaction, user) => {
+  const { name } = reaction.emoji;
+  const member = reaction.message.guild.members.cache.get(user.id);
+  if (reaction.message.id === '759623973792120842') {
+    switch (name) {
+      case ':Red:':
+        member.roles.remove('752475160707072021');
+        break;
+      case ':Orange:':
+        member.roles.remove('752493678194327632');
+        break;
+      case ':Yellow:':
+        member.roles.remove('752493680517972008');
+        break;
+      case ':Green:':
+        member.roles.remove('752493630933041203');
+        break;
+      case ':Lime:':
+        member.roles.remove('752493739276238918');
+        break;
+      case ':Blue:':
+        member.roles.remove('752493413244338189');
+        break;
+      case ':Cyan:':
+        member.roles.remove('752493735878721536');
+        break;
+      case ':Pink:':
+        member.roles.remove('752493633625784331');
+        break;
+      case ':Purple:':
+        member.roles.remove('752493688302731324');
+        break;
+      case ':White:':
+        member.roles.remove('752493685198946374');
+        break;
+      case ':Brown:':
+        member.roles.remove('752493732519084095');
+        break;
+      case ':Black:':
+        member.roles.remove('752493682711724043');
+        break;
+    
+    }
   }
 });
 
