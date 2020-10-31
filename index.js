d6a54ece2d13352e94316b4a484e12163f9e0816
BuildTools <unconfigured@null.spigotmc.org>
2020-10-31 10:48:48 +0900

    add clear command

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ client.on('ready', function() {
 
 // 서버 환영 메시지
 client.on('guildMemberAdd', member => {
-	const channel = guild.channels.find(ch => ch.name === '신병소식');
+	const channel = member.guild.channels.cache.find(channel => channel.id === '702744515802300466');
     if (!channel) return;
     const embed = new Discord.MessageEmbed()
     .setColor('#00FF00')
@@ -77,7 +77,7 @@ client.on('guildMemberAdd', member => {
 
 // 서버 퇴장 메시지
 client.on('guildMemberRemove', member => {
-    const channel = guild.channels.find(ch => ch.name === '신병소식');
+    const channel = member.guild.channels.cache.find(channel => channel.id === '702744515802300466');
     if (!channel) return;
     const embed = new Discord.MessageEmbed()
     .setColor('#FF0000')
