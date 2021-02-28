b65b8d1c709336eb60f493de3a36e435aa4c83a8
BuildTools <unconfigured@null.spigotmc.org>
2021-02-28 13:40:14 +0900

    add clear command

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ client.on('guildMemberAdd', member => {
       .setColor('#00FF00')
       .setDescription(`${member} 님께서 \`𝗚𝗔𝗠𝗝𝗔\` 길드 서버에 입장하셨습니다`)
       channel.send(embed)
+			member.roles.add('815421166318649354')
+			member.roles.add('815422085932253204')
     });
 
 // 서버 퇴장 메시지
@@ -59,4 +61,25 @@ client.on('messageDelete', async message => {
     channel.send(embed)
 });
 
-client.login(process.env.token);
+client.on('message', message => {
+	const embed = new Discord.MessageEmbed()
+	.setColor('fffaa1')
+	.setDescription(`
+	위 티어별 중 자신이 해낼 수 있는 있는 티어를 선택하고 스피드런, 데미지런 중 조건의 만족되는 사진 혹은 영상을 올리시면 됩니다.
+만약 약간의 차이로 티어를 얻지 못하게 된다면 스피드런같은 경우는 2킬, 데미지런은 8K까지 봐드립니다.`)
+	.addField('Tier 1', '스피드런 75킬 이상 / 데미지런 120K 이상')
+	.addField('Tier 2', '스피드런 85킬 이상 / 데미지런 140K 이상')
+	.addField('Tier 3', '스피드런 95킬 이상 / 데미지런 150K 이상')
+	.addField('Tier 4', '스피드런 105킬 이상 / 데미지런 170K 이상')
+	.addField('Tier 5', '스피드런 115킬 이상 / 데미지런 200K 이상')
+	.addField('Tier 6', '스피드런 130킬 이상 / 데미지런 230K 이상')
+	.addField('Tier 7', '스피드런 145킬 이상 / 데미지런 240K 이상')
+	.addField('Tier 8', '스피드런 150킬 이상 / 데미지런 250K 이상')
+	.addField('Tier 9', '스피드런 160킬 이상 / 데미지런 280K 이상')
+	.addField('Tier 10', '스피드런 170킬 이상 / 데미지런 300K 이상')
+	if(message.content === '입단조건입니다')
+	message.channel.send(embed)
+
+})
+
+client.login("ODE1NDI5MDczNDc0OTQ1MDI1.YDsRgQ.LKO1VLZj38VcXiEakqudFBH-80k");
